7d50c80a9ce25c040be24a91d4e1e1103f7371a4
Zhong Hui <zhonghui.net@gmail.com>
2021-02-23 16:33:03 +0800

    add multi-card scripts

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -1,27 +1,30 @@
 #! /bin/bash
 
 # Runs the "345M" parameter model
-
-GPUS_PER_NODE=8
+export CUDA_VISIBLE_DEVICES=0,4
+GPUS_PER_NODE=2
 # Change for multinode config
 MASTER_ADDR=localhost
 MASTER_PORT=6000
 NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+GLOBAL_BATCH_SIZE=$(($GPUS_PER_NODE*8))
 
-DATA_PATH=<Specify path and file prefix>_text_document
-CHECKPOINT_PATH=<Specify path>
+DATA_PATH=/ssd1/zhonghui03/Megatron-LM/data_dir/my-gpt2_text_document
+#DATA_PATH=/home/qiujinxuan/Megatron-LM/data_dir/
+CHECKPOINT_PATH=./checkpoints
+
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        pretrain_gpt.py \
-       --num-layers 24 \
+       --num-layers 4 \
        --hidden-size 1024 \
-       --num-attention-heads 16 \
+       --num-attention-heads 4 \
        --micro-batch-size 8 \
-       --global-batch-size 64 \
+       --global-batch-size $GLOBAL_BATCH_SIZE \
        --seq-length 1024 \
        --max-position-embeddings 1024 \
        --train-iters 500000 \
@@ -41,8 +44,7 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
        --checkpoint-activations \
-       --log-interval 100 \
+       --log-interval 1 \
        --save-interval 10000 \
        --eval-interval 1000 \
        --eval-iters 10 \
-       --fp16
